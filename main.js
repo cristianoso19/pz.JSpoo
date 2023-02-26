204c6cd2057898fb6addd0807b6efa16aeb5e689
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-26 18:28:37 -0500

    Clases en javascript

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ Student.prototype.aprobarCurso = function (nuevoCurso){
     this.cursosAprobados.push(nuevoCurso);
 } 
 
-//Instancia del prototipo
+//Instancia del prototipo 
 //Se coloca dentro del atributo '__proto__' en javascript
 const juanita = new Student(
     'Juanita Alejandra',
@@ -39,3 +39,37 @@ const juanita = new Student(
        "Curso de php" 
     ]
 )
+
+//Clases (En realidad son prototipos)
+class Student2 {
+    constructor({
+        name,
+        age,
+        cursosAprobados
+    }){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+}
+/**
+ * PATRÓN “RORO”
+
+El patrón que menciona JuanDC se llama “RORO” del Inglés “Receive an object, return an object” (Recibe un objeto, devuelve un objeto).
+
+Nos ayuda cuando tenemos muchos parámetros en nuestro constructor, ya que al instanciar la clase debemos acordarnos del orden de los parámetros y, además, no podemos tener parámetros por defecto.
+
+No obstante, si recibimos un objeto, sí podemos poner parámetros por defecto y no recibir un parámetros si fuere el caso, sin que pase nada.
+A la hora de enviar los argumentos al instanciar la clase, también debe enviarse un objeto, y otra ventaja es que esos argumentos no hace falta que estén en el mismo orden que están los parámetros del constructor. ¡Prácticamente son todo ventajas! 😃
+ */
+const miguelito = new Student2({
+    name: "Miguel",
+    age: 43,
+    cursosAprobados: [
+        "Analisis de datos",
+        "Cloud en azure",
+    ],
+});
